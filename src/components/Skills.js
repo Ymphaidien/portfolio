005ce7384007eec17037eed11079eea079fdacad
ymphaidien <ymphaidien@users.noreply.github.com>
2022-08-30 08:03:07 +0530

    first

---
+++ src/components/Skills.js
@@ -0,0 +1,71 @@
+import React, { Fragment } from 'react'
+import  { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid"
+import { skills } from "../data"
+
+
+export default function Skills() {
+    const title = "Programming Languages"
+
+    // const items = skills.filter( item => item.title === "Programming Languages").map(  x => x.list )
+    // console.log(items)
+
+    return (
+    <section id="skills">
+        <div className='container px-5 py-10 mx-auto'>
+            <div className='text-center mb-20'>
+                <ChipIcon className="w-10 inline-block mb-4" />
+                <h1 className='sm:text-4xl text-3xl font-medium title-font text-white mb-4'>
+                    Skills &amp; Technologies
+                </h1>
+                <p className='text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto'>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
+                    ipsa delectus eum quo voluptas aspernatur accusantium distinctio
+                    possimus est.   
+                </p>
+            </div>
+
+            <div className=''>
+                <div className='flex flex-wrap lg:w-full sm:mx-auto sm:mb-2 ' >
+                    <SkillAndTech   title={"Languages"} 
+                                            items={ skills.filter( item => item.title === "Programming Languages").map(x => x.list) }
+                                            />
+
+                    <SkillAndTech   title={"Web Development"} 
+                                            items={ skills.filter( item => item.title === "Web Development").map(x => x.list) } />
+
+                    <SkillAndTech   title={"Databases"} 
+                                            items={ skills.filter( item => item.title === "Databases").map(x => x.list) } />
+
+                    <SkillAndTech   title={"Technologies"} 
+                                            items={ skills.filter( item => item.title === "Technologies").map(x => x.list) } />
+
+                    <SkillAndTech   title={"Skills"} 
+                                            items={ skills.filter( item => item.title === "Skills").map(x => x.list) } />
+                </div>
+            </div>
+        </div>
+    </section>
+    )
+}
+
+function SkillAndTech({ title, items }) {
+    return(
+        <div className=' mx-3 px-3'>
+            <div className='title-font font-medium text-white mx-5 p-5' > {title}</div>
+            <div className=' '>
+                { 
+                    items[0].map( (item, i) => (
+                        <div key='item' className=' rounded flex h-full p-3'  >
+                            <BadgeCheckIcon className="text-green-400 w-6 h-6 flex-shrink-0" />
+                            <span className="ml-3">
+                                {item}
+                            </span>
+                        </div>
+                        
+                    ))
+                }
+            </div>
+        </div>
+        
+    );
+}
---
+++ src/data.js
@@ -0,0 +1,114 @@
+export const projects = [
+  {
+    title: "Study Material Sharing Platform",
+    subtitle: "Django REST Framework",
+    description:
+      "Students can share study materials to earn UNLOCKS, use UNLOCKS to view materials, Chat(Multimedia) with other user and a multitude of other features",
+    image: "./project-1.png",
+    link: "https://github.com/devsutong/minor-project",
+  },
+  {
+    title: "Elephant Detection",
+    subtitle: "Python, TensorFlow, PyTorch",
+    description:
+      "Detects Elephant in Realtime Using Artificial Intelligence (Yolov5 and Faster RCNN) for application in minimization of damage to life, crops and property in Human-Elephant Conflict prone areas",
+    image: "./project-2.png",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Meghalaya Daily",
+    subtitle: "Java, Android",
+    description:
+      "Web scrapping based news aggregator for the local Khasi tribals",
+    image: "./project-3.png",
+    link: "https://play.google.com/store/apps/details?id=com.ymphai.northeastdaily",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    list: [
+     "Java",
+     "JavaScript",
+     "Python",
+     "C++",
+     "C"
+    ]
+  },
+
+
+   { 
+    title: "Web Development",
+    list: [ 
+      "Java",
+      "JavaScript",
+      "Node.js",
+      "Django",
+      "Django REST Framework",
+      "React",
+      "Express.js",
+      "MongoDB",
+      "Material UI",
+      "Tailwind CSS"
+    ]
+  },
+
+  {
+    title: "Databases",
+    list: [
+    "MongoDB",
+    "Postgresql",
+    "MySQL"
+  ]},
+
+
+
+    {
+      title: "Technologies",
+      list: [
+      "Git",
+      "Docker"
+    ]
+  },
+
+
+
+  {
+    title: "Skills",
+    list: [
+    "Web Development",
+    "Android Development",
+    "Data Analysis & Visualization",
+    "AI & Machine Learning",
+    "Automation",
+    "Linux"]
+  }
+
+];
